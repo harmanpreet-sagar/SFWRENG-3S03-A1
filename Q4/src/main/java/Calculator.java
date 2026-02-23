bcff322032407d7f4da706a1a34d268ee0c4b24d
Jinwoo-H <jinwoo0825@gmail.com>
2026-02-22 19:48:22 -0500

    Q4f

--- Q4/src/main/java/Calculator.java
+++ Q4/src/main/java/Calculator.java
@@ -1,13 +1,17 @@
 package Q4.src.main.java;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Simple Calculator class
  * TDD: Q4b - Basic implementation until tests pass
+ * TDD: Q4f - Improving functionality so tests pass after improving the test suite
  */
 public class Calculator {
     
     /**
-     * @author Harmanpreet Singh Sagar
+     * @author Harmanpreet Singh Sagar, Jinwoo Hong
      * Divides two numbers
      * @param dividend the number to be divided
      * @param divisor the number to divide by
@@ -18,6 +22,9 @@ public class Calculator {
         if (divisor == 0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
-        return (double) dividend / divisor;
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            throw new ArithmeticException("Integer overflow");
+        }
+        return BigDecimal.valueOf(dividend).divide(BigDecimal.valueOf(divisor), 4, RoundingMode.HALF_UP).doubleValue();
     }
 }
